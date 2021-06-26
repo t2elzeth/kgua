5422714ceeb151d5f7f0ef7566e1094ab7aba4a1
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-06-26 17:55:17 +0600

    Update

--- apps/kgua_life/views.py
+++ apps/kgua_life/views.py
@@ -7,17 +7,20 @@ from .serializers import (CharitySerializer, EventSerializer,
 
 class EventListAPIView(ListAPIView):
     """Список мероприятий"""
+
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
 
 class PromotionListAPIView(ListAPIView):
     """Список акций"""
+
     queryset = Promotion.objects.all()
     serializer_class = PromotionSerializer
 
 
 class CharityListAPIView(ListAPIView):
     """Список благотворительных акций"""
+
     queryset = Charity.objects.all()
     serializer_class = CharitySerializer
