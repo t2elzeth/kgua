de5f35783e52a5f0de70205ad56a0eb8569d6ef1
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-08-29 18:22:52 +0600

    Update

--- apps/staff/filters.py
+++ apps/staff/filters.py
@@ -1,10 +1,22 @@
 import django_filters
-
+from django.conf import settings
 from . import models
 
 
 class StaffFilter(django_filters.rest_framework.FilterSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        languages = list(map(lambda el: el[0], settings.LANGUAGES))
+        query_lang = self.request.query_params.get('lang')
+
+        if query_lang not in languages:
+            query_lang = 'ru'
+
+        self.filters['full_name'].field_name += f"_{query_lang}"
+
     position = django_filters.CharFilter(field_name="position", lookup_expr='iexact')
+    full_name = django_filters.CharFilter(field_name="full_name", lookup_expr='icontains')
 
     class Meta:
         model = models.Staff
